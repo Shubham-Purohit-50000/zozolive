fd4077ee585483eee42aaa738cbc3247ac51523b
Shubham-Purohit-50000 <117735266+ShubhamSolutions1313@users.noreply.github.com>
2023-07-12 16:16:07 +0530

    nkknnjasdkjas

--- app/Http/Controllers/CallHistoryController.php
+++ app/Http/Controllers/CallHistoryController.php
@@ -58,10 +58,16 @@ class CallHistoryController extends Controller
     public function privateChatHistory(Request $req, User $user)
     {
 
-        $token_spents = TokenSpent::select('host_id', DB::raw('SUM(token) as total_token'))
+        // $token_spents = TokenSpent::select('host_id', DB::raw('SUM(token) as total_token'))
+        // ->where('user_id', $user->uuid)
+        // ->where('type', 'private_chat')
+        // ->groupBy('host_id');
+
+        $token_spents = TokenSpent::select('host_id', DB::raw('SUM(token) as total_token'), DB::raw('MAX(created_at) as latest_created_at'))
         ->where('user_id', $user->uuid)
         ->where('type', 'private_chat')
-        ->groupBy('host_id');
+        ->groupBy('host_id')
+        ->orderByDesc('latest_created_at');
 
         $formet_date = null;
 
